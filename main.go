b23ada9b790d3e06985ee97e52fec51a6710bce2
Will Lewis <will@wflewis.com>
2018-09-10 14:39:51 -0400

    Update path parsing logic

--- main.go
+++ main.go
@@ -1,67 +1,75 @@
 package main
 
 import (
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
-  "os"
-  "log"
-  "github.com/gin-gonic/gin"
-  "net/http"
-  "fmt"
-  "strings"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 const (
-  AWSLambdaFunctionVersion = "AWS_LAMBDA_FUNCTION_VERSION"
+	AWSLambdaFunctionVersion = "AWS_LAMBDA_FUNCTION_VERSION"
 )
 
 func main() {
-  _, ok := os.LookupEnv(AWSLambdaFunctionVersion)
-  if ok {
-    log.Printf("Running in AWS lambda environment, starting lambda handler.")
-    lambda.Start(AWSHandler)
-    os.Exit(0)
-  }
+	_, ok := os.LookupEnv(AWSLambdaFunctionVersion)
+	if ok {
+		log.Printf("Running in AWS lambda environment, starting lambda handler.")
+		lambda.Start(AWSHandler)
+		os.Exit(0)
+	}
 
-  log.Printf("Not running in AWS lambda environment, starting mock handler.")
-  MockHandler()
-  os.Exit(0)
+	log.Printf("Not running in AWS lambda environment, starting mock handler.")
+	MockHandler()
+	os.Exit(0)
 }
 
 func AWSHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-  log.Printf("%v", request.Path)
+	log.Printf("%v", request.Path)
 
-  splitPath := strings.Split(request.Path, "/")
+	splitPath := strings.Split(request.Path, "/")
 
-  if (splitPath[0] == ".netlify") {
-    splitPath = splitPath[2:]
+  if len(splitPath) < 1 {
+    return NotFound()
   }
 
-  if len(splitPath) < 3 {
-    return events.APIGatewayProxyResponse{
+	if splitPath[1] == ".netlify" {
+		splitPath = splitPath[2:]
+	}
+
+	if len(splitPath) < 3 {
+		return NotFound()
+	}
+
+	id := splitPath[2]
+
+	return events.APIGatewayProxyResponse{
+		Body:       "",
+		StatusCode: 302,
+		Headers: map[string]string{
+			"Location": fmt.Sprintf("https://%s.ngrok.id", id),
+		},
+	}, nil
+}
+
+func NotFound() {
+  return events.APIGatewayProxyResponse{
     Body:       "Not found\n",
     StatusCode: 404,
-  }, nil
-  }
-
-  id := splitPath[2]
-
-  return events.APIGatewayProxyResponse{
-    Body:       "",
-    StatusCode: 302,
-    Headers: map[string]string{
-      "Location": fmt.Sprintf("https://%s.ngrok.id", id),
-    },
   }, nil
 }
 
 func MockHandler() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.GET("/:id", func(c *gin.Context) {
-    path := fmt.Sprintf("https://%s.ngrok.io", c.Param("id"))
-    c.Redirect(http.StatusFound, path)
-  })
+	r.GET("/:id", func(c *gin.Context) {
+		path := fmt.Sprintf("https://%s.ngrok.io", c.Param("id"))
+		c.Redirect(http.StatusFound, path)
+	})
 
-  r.Run()
+	r.Run()
 }
